14cdc15186f4d530cff8964b164dfdc8d62c1b0d
Indigo253931 <indigo253931@gmail.com>
2016-07-25 11:42:37 -0600

    piglatin

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,19 @@
 class Word
-  def initialize(original_word)
+ def initialize(original_word)
     @original_word = original_word
   end
 
   def original_word
     return @original_word.downcase
+  
+  end
+
+    def piglatinize
+    	vowels = ["a", "e", "i", "o", "u"]
+    	consonants = ("a".."z").to_a - vowels
+
+		words = @original_word.split(" ")
+return words + "way" if words.include? (vowels[0])
+		# if consonants.include? (words[0])
   end
 end
